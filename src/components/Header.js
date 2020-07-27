1e95c3577780d3c02b4c75c2b8fe66fdb1690c2a
Roberto Barros <roberto.netto@gec.inatel.br>
2020-07-27 18:36:24 -0300

    First commit

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Header({ title, children }) {
+  return(
+    <header>
+      <h1>{tilte}</h1>
+      
+      {children}
+    </header>
+  )
+} 
